5076500617041c4f845932864a7cc8dbc2a33542
ashdeep <ashdeepupadhyay@gmail.com>
2020-09-05 20:24:08 +0530

    insert in database and recent functionality

--- main.js
+++ main.js
@@ -13,21 +13,67 @@ client.once('ready',()=>{
     console.log("First Dicord Bot is online");
 });
 
-client.on('message',message=>{
+client.on('message',async message=>{
     if(message.author.bot){
         return;
     }
-    else if(message.content.startsWith(prefixGoogle)){
+    else if(message.content.startsWith(prefixGoogle)){//if it starts with !google
         var args = message.content.slice(prefixGoogle.length);
         console.log(args);
         var command=args.trim();
         console.log(command);
+        var authorName=message.author.toString();
+       
+        let data =await botUserSearch.findOne({userName: authorName});
+        console.log("data",data);
+
+        if(data){//if author is found then just update the search array
+            console.log("update");
+            let data =await botUserSearch.update({userName:authorName},{$push:{search:command
+            }})
+            console.log("***",data)
+        }
+        else{
+            //if no author is found then insert the user with the search array
+            console.log("insert")
+            var botuser = new botUserSearch();
+            botuser.userName=authorName;
+            botuser.search.push(command);
+            botuser.save();
+            console.log("after insert")
+        }
     }
-    else if(message.content.startsWith(prefixRecent)){
+    else if(message.content.startsWith(prefixRecent)){//if it starts with !recent
         var args = message.content.slice(prefixGoogle.length);
         console.log(args);
         var command=args.trim();
         console.log(command);
+        var authorName=message.author.toString();
+
+        let data =await botUserSearch.findOne({userName: authorName});
+        console.log("data in recent",data);
+        if(data){//if data found for user
+            var da =[];
+            //iterate over all element
+            data.search.forEach(element => {
+                if(element.includes(command)){//if keyword is in search 
+                    da.push(element);
+                }
+            });
+            //if no element with the keyword
+            if(da.length==0){
+                message.channel.send("BOT: Oops!! NO Search with this keyword for you")
+            }
+            da.reverse();//reversing so the latest element can be found
+            //loop for sending the elements to the dicord
+            da.forEach(element=>{
+                message.channel.send( element);
+            });
+        }
+        else //if no data found for this user
+        {
+            message.channel.send("BOT: Oops!! NO Search with this keyword for you");  
+        }
     }
     else if(message.content==='hi'){
         message.channel.send("hey");
