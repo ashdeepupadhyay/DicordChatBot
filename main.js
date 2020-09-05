cda463161b4fef502cb6b1291779dbbbf181fda6
ashdeep <ashdeepupadhyay@gmail.com>
2020-09-05 17:22:58 +0530

    created the basic bot

---
+++ main.js
@@ -0,0 +1,22 @@
+const Discord=require("discord.js");
+
+const client= new Discord.Client();
+
+const prefix="";
+
+client.once('ready',()=>{
+    console.log("First Dicord Bot is online");
+});
+
+client.on('message',message=>{
+    if(!message.content.startsWith(prefix)||message.author.bot)
+    return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command=args.shift().toLowerCase();
+    if(command==='hi'){
+            message.channel.send("hey");
+        }
+});
+
+client.login("NzUwMzMwMTg0OTAwODA0NzI5.X049cA.ELOJE8xG8T0jaxZfMrk5AX0oJt8");
