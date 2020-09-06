f0087243dfa906c12a51278f7bd7430a4c61a425
ashdeep <ashdeepupadhyay@gmail.com>
2020-09-06 20:26:16 +0530

    made procfile

--- models/db.js
+++ models/db.js
@@ -12,31 +12,4 @@ mongoose.connect(config.mongoLabUrl,{useNewUrlParser:true,useFindAndModify: fals
     }
 });
 
-// require('./botUserSearchModel');
-
-// const { MongoClient } = require("mongodb");
-
-// // Replace the uri string with your MongoDB deployment's connection string.
-// const uri =config.mongoLabUrl;
-
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const database = client.db('sample_mflix');
-//     const collection = database.collection('movies');
-
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { title: 'Back to the Future' };
-//     const movie = await collection.findOne(query);
-
-//     console.log(movie);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 require('./botUserSearchModel');
